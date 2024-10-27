76d37e0afc827d7296de20f387bb55622dffdad8
Muhammad Salman Sarwar <muhammadsalmansarwar32@gmail.com>
2024-10-27 14:37:50 +0500

    telegram

---
+++ twitterConfig.js
@@ -0,0 +1,11 @@
+export const twitterConfig = {
+    apiKey: "YOUR_TWITTER_API_KEY",
+    apiSecretKey: "YOUR_TWITTER_API_SECRET",
+    accessToken: "YOUR_TWITTER_ACCESS_TOKEN",
+    accessSecret: "YOUR_TWITTER_ACCESS_SECRET",
+};
+
+export const telegramConfig = {
+    botToken: "8064167653:AAH9X-G4pPpB1wk2wALr3u5v44Qdiyq2i2o",
+    chatId: "515757134",
+};
